2280c390adad231777acdb9d7fd78a4edb1be1cb
Tim Marshall <tim@codingbeard.com>
2020-02-14 08:37:20 +0000

    Simplified watcher Made watcher work with docker volume

--- gulp-tasks/tasks/gulp-watcher.js
+++ gulp-tasks/tasks/gulp-watcher.js
@@ -3,93 +3,44 @@ module.exports = function (gulp, browserSync, conf, errorHandler, plugins) {
     let watcherSources = conf.assets.src;
     let jsWatchSrc = [];
     let scssWatchSrc = [];
-    let tsWatchSrc = [];
     let fontWatchSrc = [];
-    let markupWatchSrc = [];
-    let toWatch = conf.gulp.watch;
 
-    if(Array.isArray(watcherSources))
+    if (Array.isArray(watcherSources))
     {
-      for(let srcKey in watcherSources)
+      for (let srcKey in watcherSources)
       {
-        if(watcherSources.hasOwnProperty(srcKey))
+        if (watcherSources.hasOwnProperty(srcKey))
         {
           jsWatchSrc[srcKey] = watcherSources[srcKey] + ".js";
           scssWatchSrc[srcKey] = watcherSources[srcKey] + ".scss";
-          tsWatchSrc[srcKey] = watcherSources[srcKey] + ".ts";
           fontWatchSrc[srcKey] = watcherSources[srcKey] + ".+(eot|woff2|woff|ttf|svg)";
-          markupWatchSrc[srcKey] = watcherSources[srcKey] + ".phtml";
         }
       }
     }
 
-    if(Array.isArray(toWatch))
-    {
-
-      if(toWatch.includes('js'))
-      {
-
-        gulp.watch(
-          jsWatchSrc,
-          gulp.series(plugins.js, plugins.browserReload)
-        ).on('error', errorHandler);
-
-      }
-
-      if(toWatch.includes('scss'))
-      {
-
-        gulp.watch(
-          scssWatchSrc,
-          gulp.series(plugins.scss, plugins.browserReload)
-        ).on('error', errorHandler);
-
-      }
-
-      if(toWatch.includes('font'))
-      {
-
-        gulp.watch(
-          fontWatchSrc,
-          gulp.series(plugins.font, plugins.browserReload)
-        ).on('error', errorHandler);
-
-      }
-
-      if(toWatch.includes('markup'))
-      {
-
-        gulp.watch(
-          markupWatchSrc,
-          gulp.series(plugins.browserReload)
-        ).on('error', errorHandler);
-
-      }
-
-    } else
-    {
-      gulp.watch(
-        jsWatchSrc,
-        gulp.series(plugins.js, plugins.browserReload)
-      ).on('error', errorHandler);
-
-      // Styling task watcher
-      gulp.watch(
-        scssWatchSrc,
-        gulp.series(plugins.scss, plugins.browserReload)
-      ).on('error', errorHandler);
-
-      // Font task watcher
-      gulp.watch(
-        fontWatchSrc,
-        gulp.series(plugins.font, plugins.browserReload)
-      ).on('error', errorHandler);
-
-      // Template task watcher
-      gulp.watch(
-        markupWatchSrc,
-        gulp.series(plugins.browserReload)
-      ).on('error', errorHandler);
+    function sleep(cb) {
+      setTimeout(function() { cb(); }, 1000);
     }
+
+    gulp.watch(
+      jsWatchSrc,
+      {interval: 1000, usePolling: true},
+      gulp.series(plugins.js, sleep, plugins.browserReload)
+    ).on('error', errorHandler);
+
+    // Styling task watcher
+    gulp.watch(
+      scssWatchSrc,
+      {interval: 1000, usePolling: true},
+      gulp.series(plugins.scss, sleep, plugins.browserReload)
+    ).on('error', errorHandler);
+
+    // Font task watcher
+    gulp.watch(
+      fontWatchSrc,
+      {interval: 1000, usePolling: true},
+      gulp.series(plugins.font, sleep, plugins.browserReload)
+    ).on('error', errorHandler);
+
   };
 };
